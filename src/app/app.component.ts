6cea40f5351fed5b20b27c0e582bf19d849e15e7
JohannesNissenGit <nissenjohannes@web.de>
2021-03-23 19:47:24 +0100

    images added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Modul12Angular';
+export class AppComponent implements OnInit {
 
-  /**
-   * JS AREA
-   */
+  images = ['code1.jpg', 'code2.jpg', 'keyboard.jpg'];
+  currentimage = 0;
+
+  ngOnInit() {
+    this.updateimage();
+  }
+
+  updateimage() {
+
+    setInterval(() => {
+      this.currentimage ++;
+      this.currentimage = this.currentimage % this.images.length;
+    }, 8000)
+    }
 }
