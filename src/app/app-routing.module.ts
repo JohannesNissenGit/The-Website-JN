107c4132035723164c2d46a0bd145e01c7481a31
JohannesNissenGit <nissenjohannes@web.de>
2021-03-26 23:00:38 +0100

    dark mode added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DisclaimerComponent } from './disclaimer/disclaimer.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '' , component: SlideshowComponent},
+ { path: 'disclaimer' , component: DisclaimerComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+
+}
